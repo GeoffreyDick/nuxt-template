a906b4c34472564b44f6010538c4574e75f40a03
GeoffreyDick <git@geoffdick.com>
2022-08-11 07:37:27 -0600

    feat: :sparkles: Add layout and page directories

---
+++ pages/[...slug].spec.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath } from "node:url";
+import { describe, expect, it } from "vitest";
+import { setup, $fetch } from "@nuxt/test-utils";
+
+describe("pages", async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL("..", import.meta.url)),
+    server: true,
+    browser: true,
+  });
+
+  it("render index", async () => {
+    const html = await $fetch("/");
+    expect(html).toContain("Hello world");
+  });
+});
