32364781b85146eaab4235dd91bc71c913fce2ba
Geoff Dick <git@geoffdick.com>
2022-08-25 15:54:15 +0000

    fix: :bug: Enable WSS for Gitpod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,17 @@ export default defineNuxtConfig({
     // https://content.nuxtjs.org/api/configuration
   },
   tailwindcss: {
+    // https://tailwindcss.nuxtjs.org/getting-started/options
     viewer: false,
-  }
+  },
+  vite: {
+    // https://vitejs.dev/config/server-options.html
+    server: {
+      port: 443,
+      https: true,
+      hmr: {
+        protocol: 'wss'
+      }
+    },
+  },
 });
