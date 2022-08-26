f9c36fa18ade683109011f864c97a23dc3f2f654
Geoff Dick <git@geoffdick.com>
2022-08-26 16:39:05 +0000

    refactor(seo): :mag: Move default meta from nuxt.config into default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,37 +10,44 @@
 
 <script lang="ts" setup>
 const { fullPath } = useRoute()
-
 const baseURL: string = 'https://geoffdick.com'
 const canonicalURL: string = usePrettifyURL(baseURL, fullPath)
-const siteName: string = 'Fabulous Site Name'
-const siteDescription: string = 'A Fascinating Default Description.'
-const siteImage: string = 'https://picsum.photos/id/318/1200/630.jpg'
-const titleTemplate: string = `%s | ${siteName}`
+
+// TODO: Set values below
+const meta = {
+  title: 'An Interesting Title',
+  siteName: 'A Fabulous Site Name',
+  siteDescription: 'A Fascinating Default Description.',
+  img: {
+    src: 'https://picsum.photos/id/318/1200/630.jpg',
+    alt: 'A Beautiful Image'
+  },
+  twitterHandle: '@GeoffDick'
+}
 
 useHead({
-  titleTemplate,
-  meta: [
-    { name: 'description', content: siteDescription },
-    { name: 'author', content: siteName },
-    { name: 'copyright', content: siteName },
-    { name: 'robots', content: 'index, follow' },
-    // OpenGraph tags
-    { name: 'og:description', content: siteDescription },
-    { name: 'og:site_name', content: siteName },
-    { name: 'og:type', content: 'website' },
-    { name: 'og:url', content: canonicalURL },
-    { name: 'og:image', content: siteImage },
-    { name: 'og:image:url', content: siteImage },
-    { name: 'og:image:secure_url', content: siteImage },
-    { name: 'og:image:alt', content: 'A Beautiful Image' },
-    { name: 'og:image:width', content: 1200 },
-    { name: 'og:image:height', content: 630 },
-    // Twitter tags
-    { name: 'twitter:site', content: '@GeoffDick' },
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:image', content: siteImage },
-  ],
+    title: meta.title,
+    titleTemplate: `%s | ${meta.siteName}`,
+    meta: [
+      { name: 'description', content: meta.siteDescription },
+      { name: 'author', content: meta.siteName },
+      { name: 'copyright', content: meta.siteName },
+      { name: 'robots', content: 'index, follow' },
+      // OpenGraph tags
+      { hid: 'og:site_name', property: 'og:site_name', content: meta.siteName },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: canonicalURL },
+      { hid: 'og:image', property: 'og:image', content: meta.img.src },
+      { hid: 'og:image:url', property: 'og:image', content: meta.img.src },
+      { hid: 'og:image:secure_url', property: 'og:image:secure_url', content: meta.img.src },
+      { hid: 'og:image:alt', property: 'og:image:alt', content: meta.img.alt },
+      { hid: 'og:image:width', property: 'og:image:width', content: 1200 },
+      { hid: 'og:image:height', property: 'og:image:height', content: 630 },
+      // Twitter tags
+      { hid: 'twitter:site', name: 'twitter:site', content: meta.twitterHandle },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:image', name: 'twitter:image', content: meta.img.src },
+    ],
   link: [
     { rel: 'canonical', href: canonicalURL }
   ]
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,41 +2,6 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  head: {
-    titleTemplate: '%s | A Fabulous Site Name',
-    meta: [
-      // OpenGraph tags
-      { hid: 'og:site_name', property: 'og:site_name', content: 'A Fabulous Site Name' },
-      { hid: 'og:type', property: 'og:type', content: 'website' },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: 'https://picsum.photos/id/318/1200/630.jpg'
-      },
-      {
-        hid: 'og:image:secure_url',
-        property: 'og:image:secure_url',
-        content: 'https://picsum.photos/id/318/1200/630.jpg'
-      },
-      {
-        hid: 'og:image:alt',
-        property: 'og:image:alt',
-        content: 'A Beautiful Image'
-      },
-      // Twitter tags
-      { hid: 'twitter:site', name: 'twitter:site', content: '@GeoffDick' },
-      {
-        hid: 'twitter:card',
-        name: 'twitter:card',
-        content: 'summary_large_image'
-      },
-      {
-        hid: 'twitter:image',
-        name: 'twitter:image',
-        content: 'https://picsum.photos/id/318/1200/630.jpg'
-      },
-    ],
-  },
   typescript: {
     shim: false,
   },
